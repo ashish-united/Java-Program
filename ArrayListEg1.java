1348799e8ee9096106fdd4e06047616d39f1a2c5
ashish-united <ag2064931@gmail.com>
2026-04-09 23:26:54 +0530

    java Program

---
+++ ArrayListEg1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class ArrayListEg1 {
+    public static void main(String[] args) {
+        ArrayList<String> li=new ArrayList<String>();
+        li.add("Ashish");
+        li.add("Aryan");
+        li.add("Arya");
+        li.add("Asutosh");
+        li.add("Archit");
+        System.out.println(li);
+    }
+}
