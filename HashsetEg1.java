f7f48b6dd43fe6e7ab283b3c8e57f3c57d55ea8c
ashish-united <ag2064931@gmail.com>
2026-04-12 22:48:39 +0530

    Java Program

---
+++ HashsetEg1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class HashsetEg1 {
+    public static void main(String args[])  {
+        HashSet<String> set = new HashSet<>();
+        set.add("One");
+        set.add("Two");
+        set.add("Three");
+        set.add("Four");
+        set.add("Five");
+        Iterator<String> i=set.iterator();
+    while(i.hasNext()){
+        System.out.println(i.next());
+    }     }
+}
